6904e74509caa46d6b7e35fde174be882de0b1ab
millerrrrrrr <mler1934@gmail.com>
2025-10-05 12:44:56 +0800

    edited

--- resources/views/qrTester/index.blade.php
+++ resources/views/qrTester/index.blade.php
@@ -3,19 +3,17 @@
 @section('pagetitle', 'Qr Tester')
 @section('main')
 
-<div class="flex flex-col items-center justify-center h-[80%]">
-    <h1 class="text-2xl font-bold mb-6">QR Code Scanner</h1>
+    <div class="flex flex-col items-center justify-center h-[80%]">
+        <h1 class="text-2xl font-bold mb-6">QR Code Scanner</h1>
 
-    <div id="qr-reader" class="mb-4 w-[500px] max-h-[80vh]"></div>
+        <div id="qr-reader" class="mb-4 w-[500px] max-h-[80vh]"></div>
 
-    <div id="qr-result" class="hidden mt-4 text-left"> 
-        <p class="text-lg">Scanned LRN: <span id="scanned-data" class="font-semibold"></span></p>
+        <div id="qr-result" class="hidden mt-4 text-left">
+            <p class="text-lg">Scanned LRN: <span id="scanned-data" class="font-semibold"></span></p>
+        </div>
+        >
+        <div id="user-info" class="mt-4 text-left">
+        </div>
     </div>
->
-    <div id="user-info" class="mt-4 text-left"> 
-    </div>
-</div>
 
 @endsection
-
-
