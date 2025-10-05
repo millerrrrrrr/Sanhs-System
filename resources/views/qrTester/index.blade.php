9fc95647e24a1458932669a4189d4c05b7352bda
millerrrrrrr <mler1934@gmail.com>
2025-10-05 11:58:36 +0800

    edited

--- resources/views/qrTester/index.blade.php
+++ resources/views/qrTester/index.blade.php
@@ -6,17 +6,13 @@
 <div class="flex flex-col items-center justify-center h-[80%]">
     <h1 class="text-2xl font-bold mb-6">QR Code Scanner</h1>
 
-    <!-- The scanner will be rendered inside this div with max height to prevent overflow -->
     <div id="qr-reader" class="mb-4 w-[500px] max-h-[80vh]"></div>
 
-    <!-- Scanned QR code result will show here -->
-    <div id="qr-result" class="hidden mt-4 text-left"> <!-- Change text-center to text-left here -->
+    <div id="qr-result" class="hidden mt-4 text-left"> 
         <p class="text-lg">Scanned LRN: <span id="scanned-data" class="font-semibold"></span></p>
     </div>
-
-    <!-- User info will be displayed here below the scanner -->
-    <div id="user-info" class="mt-4 text-left"> <!-- Change text-center to text-left here -->
-        <!-- User info will be dynamically populated here after scanning -->
+>
+    <div id="user-info" class="mt-4 text-left"> 
     </div>
 </div>
 
