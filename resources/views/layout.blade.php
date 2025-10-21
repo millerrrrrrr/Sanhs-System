943f367abc5acf902c12cdf713bd4cfd15152faf
millerrrrrrr <mler1934@gmail.com>
2025-10-21 09:19:51 +0800

    MEMEME

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
-    {{-- @vite('resources/css/app.css') --}}
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
